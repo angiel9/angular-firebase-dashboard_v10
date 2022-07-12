c4e3693ab8823b1d638a9c3b1451eb7636af1b35
angiel9 <108388034+angiel9@users.noreply.github.com>
2022-07-11 23:39:41 -0500

    TEST

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'dashboard-cbb32',
+    appId: '1:283492349102:web:967f0d38a681b80839a93f',
+    databaseURL: 'https://dashboard-cbb32-default-rtdb.firebaseio.com',
+    storageBucket: 'dashboard-cbb32.appspot.com',
+    locationId: 'us-west2',
+    apiKey: 'AIzaSyAZh2ZacSOm42iEfm3T0JA1hL1vU0q7OxM',
+    authDomain: 'dashboard-cbb32.firebaseapp.com',
+    messagingSenderId: '283492349102',
+  },
   production: false
 };
 
